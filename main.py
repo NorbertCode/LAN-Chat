28e1aebd82c4b6c07ed600007fb93b1d330ca77b
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2023-06-16 20:32:35 +0200

    prepared for testing

--- main.py
+++ main.py
@@ -9,14 +9,11 @@ FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "disconnect"
 
 # TODO: make this idiot-proof
-def amogus():
-    for i in range(0, 10):
-        print("amogus")
-
 recipient_ip = input("IP Address: ")
 
 localhost = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 localhost.bind((LOCALHOST_IP, PORT))
+localhost.connect((recipient_ip, PORT))
 
 def ReceiveMessage(connection, address):
     while True:
@@ -24,13 +21,23 @@ def ReceiveMessage(connection, address):
         if message_len:
             message_len = int(message_len)
             message = connection.recv(message_len).decode(FORMAT)
+
             if message == DISCONNECT_MESSAGE:
                 break
-            
+
             print(f"<{address}>: {message}")
+    connection.close()
+
+def SendMessage(message):
+    message = message.encode(FORMAT)
+    message_length = str(len(message)).encode(FORMAT)
+    message_length += b' ' * (HEADER - len(message_length))
+    localhost.send(message_length)
+    localhost.send(message)
 
 localhost.listen()
 while True:
+    SendMessage(input("Enter your message: "))
     connection, address = localhost.accept()
     thread = threading.Thread(target=ReceiveMessage, args=(connection, address))
     thread.start()
