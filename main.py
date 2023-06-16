562e89df3f42f9dcaf0f1c412c4ccdf5b11147b7
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2023-06-16 19:45:31 +0200

    base send-receive system

---
+++ main.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+LOCALHOST_IP = "127.0.0.1"
+PORT = 5050
+
+HEADER = 64
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "disconnect"
+
+# TODO: make this idiot-proof
+def amogus():
+    for i in range(0, 10):
+        print("amogus")
+
+recipient_ip = input("IP Address: ")
+
+localhost = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+localhost.bind((LOCALHOST_IP, PORT))
+
+def ReceiveMessage(connection, address):
+    while True:
+        message_len = connection.recv(HEADER).decode(FORMAT)
+        if message_len:
+            message_len = int(message_len)
+            message = connection.recv(message_len).decode(FORMAT)
+            if message == DISCONNECT_MESSAGE:
+                break
+            
+            print(f"<{address}>: {message}")
+
+localhost.listen()
+while True:
+    connection, address = localhost.accept()
+    thread = threading.Thread(target=ReceiveMessage, args=(connection, address))
+    thread.start()
+    
