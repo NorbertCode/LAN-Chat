c87c41153621cc39b5edb5a6bdec6e2d9df542f2
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2023-06-16 21:03:11 +0200

    trying something else

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-LOCALHOST_IP = "127.0.0.1"
+LOCALHOST_IP = socket.gethostbyname(socket.gethostname())
 PORT = 5050
 
 HEADER = 64
@@ -9,13 +9,12 @@ FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "disconnect"
 
 localhost = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-localhost.bind((LOCALHOST_IP, PORT))
 
 # TODO: make this idiot-proof
 recipient_ip = input("IP Address: ")
-localhost.connect((recipient_ip, PORT))
 
 def ReceiveMessage(connection, address):
+    localhost.bind((LOCALHOST_IP, PORT))
     while True:
         message_len = connection.recv(HEADER).decode(FORMAT)
         if message_len:
@@ -29,11 +28,13 @@ def ReceiveMessage(connection, address):
     connection.close()
 
 def SendMessage(message):
+    localhost.connect((recipient_ip, PORT))
     message = message.encode(FORMAT)
     message_length = str(len(message)).encode(FORMAT)
     message_length += b' ' * (HEADER - len(message_length))
     localhost.send(message_length)
     localhost.send(message)
+    localhost.close()
 
 localhost.listen()
 while True:
