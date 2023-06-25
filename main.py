e3afc3abbe38e72b653fd94f581c9449789c5b7b
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2023-06-25 19:44:23 +0200

    added option to disconnect

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ import sender
 root = tk.Tk()
 root.geometry("400x500")
 
-# Create an entry box to connect to an IP
+# Entry box to connect to an IP
 ip_field = tk.Entry(root, width=50)
 ip_field.pack(pady=10)
 
+# Container for the buttons
+button_container = tk.Frame(root)
+button_container.pack()
+
 # Button to connect to an IP
-connect_button = tk.Button(root, text="Connect", width=10)
-connect_button.pack()
+connect_button = tk.Button(button_container, text="Connect", width=10)
+connect_button.pack(side="left", padx=10)
+
+# Button to disconnect
+disconnect_button = tk.Button(button_container, text="Disconnect", width=10)
+disconnect_button.pack(side="right", padx=10)
 
 # Textbox to display messages
 messages = tk.Text(root, state="disabled", height=20, width=50)
@@ -42,6 +50,8 @@ connect_button.configure(command=lambda: send.Start(ip_field.get()))
 
 entry.bind("<Return>", (lambda event: send.SendMessage(entry.get())))
 send_button.configure(command=lambda: send.SendMessage(entry.get()))
+
+disconnect_button.configure(command=send.Disconnect)
 
 receive = receiver.Receiver(EnterMessage)
 receiverThread = threading.Thread(target=receive.Start, daemon=True)
--- sender.py
+++ sender.py
@@ -7,49 +7,50 @@ class Sender:
     def __init__(self, ShowMessageFunc):
         self.connected = False
         self.ShowMessage = ShowMessageFunc
-        self.localhost = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         atexit.register(self.Disconnect) # If the user exits send a disconnect message
 
     def SendData(self, length, data):
-        # First send a message defining the length of the actual message
-        self.localhost.send(length)
-        self.localhost.send(data)
+        try:
+            # First send a message defining the length of the actual message
+            self.localhost.send(length)
+            self.localhost.send(data)
+        except Exception as e:
+            self.ShowMessage(str(e))
 
     def SendMessage(self, message, showLocally = True):
-        try:
-            # Show it in the entry box
-            if showLocally:
-                self.ShowMessage(message)
+        # Show it in the entry box
+        if showLocally:
+            self.ShowMessage(message)
 
-            message = message.encode(util.FORMAT)
+        message = message.encode(util.FORMAT)
 
-            message_length = str(len(message)).encode(util.FORMAT)
-            message_length += b' ' * (util.HEADER - len(message_length))
+        message_length = str(len(message)).encode(util.FORMAT)
+        message_length += b' ' * (util.HEADER - len(message_length))
 
-            sendThread = threading.Thread(target=self.SendData, args=(message_length, message))
-            sendThread.start()
-            
-        except Exception as e:
-            self.ShowMessage(str(e))
+        sendThread = threading.Thread(target=self.SendData, args=(message_length, message))
+        sendThread.start()
 
     def Connect(self, ip):
-        try:
-            self.localhost.connect((ip, util.PORT))
-            self.connected = True
+        if not self.connected:
+            try:
+                self.localhost.connect((ip, util.PORT))
+                self.connected = True
 
-            self.ShowMessage(f"Connected to {ip}")
-            self.SendMessage(util.JOINED_MESSAGE, False)
-        except Exception as e:
-            self.ShowMessage(str(e))
+                self.ShowMessage(f"Connected to {ip}")
+                self.SendMessage(util.JOINED_MESSAGE, False)
+            except Exception as e:
+                self.ShowMessage(str(e))
 
     def Disconnect(self):
         if self.connected:
-            self.SendMessage(util.DISCONNECT_MESSAGE)
+            self.connected = False
+            self.ShowMessage("Disconnecting...")
+            self.SendMessage(util.DISCONNECT_MESSAGE, False)
 
     def Start(self, ip):
         self.ShowMessage("Connecting...")
-        
+        self.localhost = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         # Handle connecting on a separate thread, so the program still responds while connecting
         connectThread = threading.Thread(target=lambda: self.Connect(ip))
         connectThread.start()
